f23c85d4328b992ee9fd1829b3dfc93d0aa02f79
kalinga890 <kalingaa69@gmail.com>
2024-10-17 10:56:31 +0530

    Variables file

---
+++ VariableExamples.java
@@ -0,0 +1,75 @@
+public class VariableExamples{
+	public static  void main(String[] args) {
+
+		// Declaration, and Initialization
+
+		int age=20;
+		    age=50;  //assign value
+		System.out.println(age);
+
+		int price=200;
+		System.out.println(price);
+
+		int computer=46000;
+		System.out.println(computer);
+
+		int table=300;
+		System.out.println(table);
+
+		int keyboard=500;
+		System.out.println(keyboard);
+
+		int mouse=100;
+		System.out.println(mouse);
+
+		int mobile=10000;
+		System.out.println(mobile);
+
+		int bike=20000;
+		System.out.println(bike);
+
+		int cykel=5000;
+		System.out.println(cykel);
+
+		int book=50;
+		System.out.println(book);
+
+		int pen=15;
+		System.out.println(pen);
+
+		int bag=400;
+		System.out.println(bag);
+
+		int mango=80;
+		System.out.println(mango);
+
+		int watch=2500;
+		System.out.println(watch);
+
+		int bed=7000;
+		System.out.println(bed);
+
+		int mobilecharger=350;
+		System.out.println(mobilecharger);
+
+		int tv=30000;
+		System.out.println(tv);
+
+		int bat=250;
+		System.out.println(bat);
+
+		int ball=35;
+		System.out.println(ball);
+
+		int cake=600;
+		System.out.println(cake);
+
+		int apple=150;
+		System.out.println(apple);
+
+	}
+}
+
+	
+
+	
