36800d284545e6f1d411ce6a2318381a6d48a6b9
kalinga890 <kalingaa69@gmail.com>
2024-10-17 11:14:16 +0530

    Datatypes file

---
+++ Datatypes.java
@@ -0,0 +1,365 @@
+ public class Datatypes{
+ 	 public static void main(String[] args) {
+
+ 	 	// byte variable example
+
+ 	 	byte Targettemperature=22;
+ 	 	System.out.println(Targettemperature);
+
+ 	 	byte currentTemperature=20;
+ 	 	System.out.println(currentTemperature);
+
+ 	 	byte fanSpeed=3;
+ 	 	System.out.println(fanSpeed);
+
+ 	 	byte humidityLevel=45;
+ 	 	System.out.println(humidityLevel);
+
+ 	 	byte heatingPower=70;
+ 	 	System.out.println(heatingPower);
+
+ 	 	byte coolingPower=-50;
+ 	 	System.out.println(coolingPower);
+
+ 	 	byte energySavingMode=1;
+ 	 	System.out.println(energySavingMode);
+
+ 	 	byte motionDetected=0;
+ 	 	System.out.println(motionDetected);
+
+ 	 	byte errorCode=-1;
+ 	 	System.out.println(errorCode);
+
+ 	 	byte systemStatus=1;
+ 	 	System.out.println(systemStatus);
+
+ 	 	// short variable examples
+
+              short riversInindia=400;
+ 	 	System.out.println(riversInindia);
+
+ 	 	short publicLibrariesInkarnataka=6890;
+ 	 	System.out.println(publicLibrariesInkarnataka);
+
+ 	 	short governamenthospitalInindia=23581;
+ 	 	System.out.println(governamenthospitalInindia);
+
+ 	 	short taluksInkarnataka=240;
+ 	 	System.out.println(taluksInkarnataka);
+
+ 	 	short lionspopulationInindia=674;
+ 	 	System.out.println(lionspopulationInindia);
+
+ 	 	short tigerpopulationInindia=3925;
+ 	 	System.out.println(tigerpopulationInindia);
+
+ 	 	short airportsInindia=137;
+ 	 	System.out.println(airportsInindia);
+
+ 	 	short studentInschool=2000;
+ 	 	System.out.println(studentInschool);
+
+ 	 	short universitiesInindia=1113;
+ 	 	System.out.println(universitiesInindia);
+
+ 	 	short engineeringcollegesInindia=8876;
+ 	 	System.out.println(engineeringcollegesInindia);
+
+              // int variable examples
+
+              int  GalaxyCount=2147483647;
+              System.out.println(GalaxyCount);
+
+              int CityPopulation=2147483647;
+              System.out.println(CityPopulation);
+
+              int DataStorage=1073741824;
+              System.out.println(DataStorage);
+
+              int WebPageViews=1073741811;
+              System.out.println(WebPageViews);
+
+              int AstronomicalDistance=2147483647;
+              System.out.println(AstronomicalDistance);
+
+              int FinancialTransactions=1000000000;
+              System.out.println(FinancialTransactions);
+
+              int DatabaseRecords=2147483647;
+              System.out.println(DatabaseRecords);
+
+              int  PixelResolution=2097152000;
+              System.out.println(PixelResolution);
+
+              int  NetworkUsers=1000000000;
+              System.out.println(NetworkUsers);
+
+              int StarCount=1000000000;
+              System.out.println(StarCount);
+
+              // long variables examples
+
+              long  ParticleCollision=47223664859031000L;
+              System.out.println(ParticleCollision);
+
+              long  DarkMatterDensity=70368744177664L;
+              System.out.println(DarkMatterDensity);
+
+              long SpacetimeCoordinates=9223372036854775807L;
+              System.out.println(SpacetimeCoordinates);
+
+              long  EventHorizonRadius= 792281625142643375L;
+              System.out.println(EventHorizonRadius);
+
+              long DataStorageCapacity= 879609302220800000l;
+              System.out.println(DataStorageCapacity);
+
+              long UniversalTime= 9223372036854775807L;
+              System.out.println(UniversalTime);
+
+              long  SolarSystemAge= 47223664859031000L;
+              System.out.println(SolarSystemAge);
+
+              long BlackHoleEntropy= 9223372036854775807L;
+              System.out.println(BlackHoleEntropy);
+
+              long  QuantumFluctuation= -70368744177664L;
+              System.out.println(QuantumFluctuation);
+
+              long GalacticCenterDistance = 9223372036854775807L;
+
+              System.out.println(GalacticCenterDistance);
+
+              // float variables examples
+
+               float GravityAcceleration = 9.81f;
+               System.out.println(GravityAcceleration);
+
+               float EarthRadius = 6371.0f;
+               System.out.println(EarthRadius);
+
+                float MoonDistance = 384400.0f;
+                System.out.println(MoonDistance);
+
+                float SolarMass = 1.989e30f;
+                System.out.println(SolarMass);
+
+                 float AbsoluteZero = -273.15f;
+                 System.out.println(AbsoluteZero);
+
+                 float BoilingPoint = 100.0f;
+                 System.out.println(BoilingPoint);
+
+                  float FreezingPoint = 0.0f;
+                  System.out.println(FreezingPoint);
+
+                  float KilometerToMile = 0.621371f;
+                    System.out.println(KilometerToMile);
+
+                    float Pi = 3.14159f;
+                     System.out.println(Pi);
+
+                      float EulerNumber = 2.71828f;
+                      System.out.println(EulerNumber);
+
+
+                      // double variable examples
+
+                      double AtomicMassUnit = 1.66053907e-27;
+                      System.out.println(AtomicMassUnit);
+
+                       double AvogadroConstant = 6.02214076e23;
+                        System.out.println(AvogadroConstant);
+
+                          double BoltzmannConstant = 1.38064852e-23;
+                           System.out.println(BoltzmannConstant);
+
+                            double PlanckConstant = 6.62607015e-34;
+                             System.out.println(PlanckConstant);
+
+                               double GoldenRatio = 1.61803398875;
+                               System.out.println(GoldenRatio);
+
+                                  double RocketThrust = 1.2e6;
+
+                                 System.out.println(RocketThrust);
+
+                                  double ProtonMass = 1.67262171e-27;
+                                   System.out.println(ProtonMass);
+
+                                    double SpeedOfLight = 299792458.0;
+                                    System.out.println(SpeedOfLight);
+
+                                     double GasConstant = 8.314462618;
+                                      System.out.println(GasConstant);
+
+                                       double CatalanConstant = 0.9159655942;
+                                       System.out.println(CatalanConstant);
+
+
+            // char variable examples 
+
+                                        char FirstLetter = 'A';
+                                          System.out.println(FirstLetter);
+
+                                          char ExclamationMark = '!';
+                                          System.out.println(ExclamationMark);
+
+                                           char Zero = '0';
+                                            System.out.println(Zero);
+
+                                             char CarriageReturn = '\r';
+                                              System.out.println(CarriageReturn);
+
+                                                char NewLine = '\n';
+                                                 System.out.println(NewLine);
+
+                                                   char Nine = '9';
+                                                     System.out.println(Nine);
+
+                                                      char Copyright = '\u00A9';
+                                                      System.out.println(Copyright);
+
+                                                       char Registered = '\u00AE';
+                                                         System.out.println(Registered);
+
+                                                         char Trademark = '\u2122';
+                                                         System.out.println(Trademark);
+
+                                                          char Hash = '#';
+                                                            System.out.println(Hash);
+
+
+               
+               // boolean variable examples
+                                                            boolean javaIseasy=false;
+                                                             System.out.println(javaIseasy);
+
+                                                             boolean tomorrowIsholiday=true;
+                                                              System.out.println(tomorrowIsholiday);
+
+                                                                boolean IsGameStarted = true;
+
+                                                               System.out.println(IsGameStarted);
+
+                                                               boolean nationalBirdbaldeagle=false;
+                                                                System.out.println(nationalBirdbaldeagle);
+
+                                                                 boolean IsOnline = true;
+                                                                   System.out.println(IsOnline);
+
+                                                                     boolean IsVerified = true;
+                                                                      System.out.println(IsVerified);
+
+                                                                      boolean todayclassIsthere=true;
+                                                                      System.out.println(todayclassIsthere);
+
+                                                                      boolean todayapjabdulkalambirthday=false;
+                                                                       System.out.println(todayapjabdulkalambirthday);
+
+                                                                       boolean manuBhekermarried=false;
+                                                                       System.out.println(manuBhekermarried);
+
+                                                                       boolean firstPresidentranjendraprasad=true;
+                                                                        System.out.println(firstPresidentranjendraprasad);
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+ 	 		
+ 	 	}
+
+ 		
+ 	}
+ 
