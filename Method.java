b2e6facfa6bf8baf06b09bce4c9ae163f6c38bc1
kalinga890 <kalingaa69@gmail.com>
2024-10-18 15:06:02 +0530

    Method file

---
+++ Method.java
@@ -0,0 +1,26 @@
+public class Method{
+
+	public static void main(String[] args) {
+	
+	
+	subject();
+	subject();
+	subject();	
+
+	village();
+	village();
+	village();
+
+	}
+
+	
+	public static void subject(){
+		System.out.println("my favorite subject is maths");
+	}
+
+
+	public static void village(){
+
+		System.out.println("balakundi");
+	}
+}
