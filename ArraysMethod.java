99def60ab1c9c20278df81ee4e1a2eb228024ce0
kalinga890 <kalingaa69@gmail.com>
2024-11-12 19:12:11 +0530

    ArraysMethod

---
+++ ArraysMethod.java
@@ -0,0 +1,414 @@
+public class ArraysMethod {
+
+	public static void main(String[] args) {
+		
+		byte[] marks  = {20,40,50,10,30,60,70,90,100,25};
+
+		marks[0]= 40;
+		marks[1]= 50;
+		marks[2]= 90;
+
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		System.out.println(marks[2]);
+		System.out.println(marks[3]);
+		System.out.println(marks[4]);
+		System.out.println(marks[5]);
+		System.out.println(marks[6]);
+		System.out.println(marks[7]);
+		System.out.println(marks[8]);
+		System.out.println(marks[9]);
+	
+
+		// key value method
+		
+		
+
+		byte[] heatingPower = new byte[10];
+
+		heatingPower[0]= 20;
+		heatingPower[1]= 30;
+		heatingPower[2]= 40;
+		heatingPower[3]= 50;
+		heatingPower[4]= 60;
+		heatingPower[5]= 70;
+		heatingPower[6]= 80;
+		heatingPower[7]= 90;
+		heatingPower[8]= 100;
+		heatingPower[9]= 45;
+
+		System.out.println(heatingPower[0]);
+		System.out.println(heatingPower[1]);
+		System.out.println(heatingPower[2]);
+		System.out.println(heatingPower[3]);
+		System.out.println(heatingPower[4]);
+		System.out.println(heatingPower[5]);
+		System.out.println(heatingPower[6]);
+		System.out.println(heatingPower[7]);
+		System.out.println(heatingPower[8]);
+		System.out.println(heatingPower[9]);
+
+
+		short[] riversInindia= {150,200,300,500,600,700,350,250,555,444};
+
+		riversInindia[0]= 222;
+		riversInindia[1]= 323;
+		riversInindia[2]= 443;
+
+		System.out.println(riversInindia[0]);
+		System.out.println(riversInindia[1]);
+		System.out.println(riversInindia[2]);
+		System.out.println(riversInindia[3]);
+		System.out.println(riversInindia[4]);
+		System.out.println(riversInindia[5]);
+		System.out.println(riversInindia[6]);
+		System.out.println(riversInindia[7]);
+		System.out.println(riversInindia[8]);
+		System.out.println(riversInindia[9]);
+
+		// key value method
+
+		short[] airportsInindia= new short[10];
+
+		airportsInindia[0]=230;
+		airportsInindia[1]=530;
+		airportsInindia[2]=630;
+		airportsInindia[3]=730;
+		airportsInindia[4]=250;
+		airportsInindia[5]=260;
+		airportsInindia[6]=990;
+		airportsInindia[7]=450;
+		airportsInindia[8]=670;
+		airportsInindia[9]=740;
+
+		System.out.println(airportsInindia[0]);
+		System.out.println(airportsInindia[1]);
+		System.out.println(airportsInindia[2]);
+		System.out.println(airportsInindia[3]);
+		System.out.println(airportsInindia[4]);
+		System.out.println(airportsInindia[5]);
+		System.out.println(airportsInindia[6]);
+		System.out.println(airportsInindia[7]);
+		System.out.println(airportsInindia[8]);
+		System.out.println(airportsInindia[9]);
+
+
+		int[] CityPopulation = {10000,24456,345673,24689,123455,34563,56743,28943,34763,76541};
+
+
+		CityPopulation[0]=33452;
+		CityPopulation[1]=13252;
+		CityPopulation[2]=23452;
+		
+
+		System.out.println(CityPopulation[0]);
+		System.out.println(CityPopulation[1]);
+		System.out.println(CityPopulation[2]);
+		System.out.println(CityPopulation[3]);
+		System.out.println(CityPopulation[4]);
+		System.out.println(CityPopulation[5]);
+		System.out.println(CityPopulation[6]);
+		System.out.println(CityPopulation[7]);
+		System.out.println(CityPopulation[8]);
+		System.out.println(CityPopulation[9]);
+
+		// key value method
+
+		int[] DataStorage= new int[10];
+
+		DataStorage[0]=45673;
+		DataStorage[1]=55683;
+		DataStorage[2]=35673;
+		DataStorage[3]=55673;
+		DataStorage[4]=65673;
+		DataStorage[5]=75673;
+		DataStorage[6]=85673;
+		DataStorage[7]=43473;
+		DataStorage[8]=65673;
+		DataStorage[9]=75673;
+
+		System.out.println(DataStorage[0]);
+		System.out.println(DataStorage[1]);
+		System.out.println(DataStorage[2]);
+		System.out.println(DataStorage[3]);
+		System.out.println(DataStorage[4]);
+		System.out.println(DataStorage[5]);
+		System.out.println(DataStorage[6]);
+		System.out.println(DataStorage[7]);
+		System.out.println(DataStorage[8]);
+		System.out.println(DataStorage[9]);
+
+		long[] ParticleCollision= {12345345l,23563233l,34563273l,4327843l,23456278l,7643822l,98635342l,74683748l,8456329l,666324l};
+
+		ParticleCollision[0]=33333222l;
+		ParticleCollision[1]=43434342l;
+		ParticleCollision[3]=89334533l;
+
+		System.out.println(ParticleCollision[0]);
+		System.out.println(ParticleCollision[1]);
+		System.out.println(ParticleCollision[2]);
+		System.out.println(ParticleCollision[3]);
+		System.out.println(ParticleCollision[4]);
+		System.out.println(ParticleCollision[5]);
+		System.out.println(ParticleCollision[6]);
+		System.out.println(ParticleCollision[7]);
+		System.out.println(ParticleCollision[8]);
+		System.out.println(ParticleCollision[9]);
+
+		// key value method
+
+		long[] DataStorageCapacity=new long[10];
+
+		DataStorageCapacity[0]=32342682l;
+		DataStorageCapacity[1]=33352682l;
+		DataStorageCapacity[2]=33342682l;
+		DataStorageCapacity[3]=63342682l;
+		DataStorageCapacity[4]=33342682l;
+		DataStorageCapacity[5]=32346682l;
+		DataStorageCapacity[6]=33382682l;
+		DataStorageCapacity[7]=13342682l;
+		DataStorageCapacity[8]=43342682l;
+		DataStorageCapacity[9]=36342672l;
+
+
+
+		System.out.println(DataStorageCapacity[0]);
+		System.out.println(DataStorageCapacity[1]);
+		System.out.println(DataStorageCapacity[2]);
+		System.out.println(DataStorageCapacity[3]);
+		System.out.println(DataStorageCapacity[4]);
+		System.out.println(DataStorageCapacity[5]);
+		System.out.println(DataStorageCapacity[6]);
+		System.out.println(DataStorageCapacity[7]);
+		System.out.println(DataStorageCapacity[8]);
+		System.out.println(DataStorageCapacity[9]);
+
+		float[] BoilingPoint= {25.4f,35.3f,20.05f,80.65f,30.3f,40.75f,48.3f,50.43f,40.32f,75.22f};
+
+		BoilingPoint[0]=80.22f;
+		BoilingPoint[1]=32.23f;
+		BoilingPoint[3]=78.85f;
+
+		System.out.println(BoilingPoint[0]);
+		System.out.println(BoilingPoint[1]);
+		System.out.println(BoilingPoint[2]);
+		System.out.println(BoilingPoint[3]);
+		System.out.println(BoilingPoint[4]);
+		System.out.println(BoilingPoint[5]);
+		System.out.println(BoilingPoint[6]);
+		System.out.println(BoilingPoint[7]);
+		System.out.println(BoilingPoint[8]);
+		System.out.println(BoilingPoint[9]);
+
+		// key value method
+
+		float[] FreezingPoint= new float[10];
+
+		FreezingPoint[0]=0.25f;
+		FreezingPoint[1]=0.35f;
+		FreezingPoint[2]=0.45f;
+		FreezingPoint[3]=0.55f;
+		FreezingPoint[4]=0.65f;
+		FreezingPoint[5]=0.75f;
+		FreezingPoint[6]=0.85f;
+		FreezingPoint[7]=0.59f;
+		FreezingPoint[8]=0.24f;
+		FreezingPoint[9]=0.26f;
+
+		System.out.println(FreezingPoint[0]);
+		System.out.println(FreezingPoint[1]);
+		System.out.println(FreezingPoint[2]);
+		System.out.println(FreezingPoint[3]);
+		System.out.println(FreezingPoint[4]);
+		System.out.println(FreezingPoint[5]);
+		System.out.println(FreezingPoint[6]);
+		System.out.println(FreezingPoint[7]);
+		System.out.println(FreezingPoint[8]);
+		System.out.println(FreezingPoint[9]);
+
+
+		double[] AtomicMassUnit={1.2345d,2.345d,4.432d,4.123d,8.236d,3.483d,5.782d,6.143d,2.987d,8.543d};
+
+		AtomicMassUnit[0]=23.444d;
+		AtomicMassUnit[1]=67.432d;
+		AtomicMassUnit[4]=66.723d;
+
+		System.out.println(AtomicMassUnit[0]);
+		System.out.println(AtomicMassUnit[1]);
+		System.out.println(AtomicMassUnit[2]);
+		System.out.println(AtomicMassUnit[3]);
+		System.out.println(AtomicMassUnit[4]);
+		System.out.println(AtomicMassUnit[5]);
+		System.out.println(AtomicMassUnit[6]);
+		System.out.println(AtomicMassUnit[7]);
+		System.out.println(AtomicMassUnit[8]);
+		System.out.println(AtomicMassUnit[9]);
+
+		// key value method
+
+		double[] BoltzmannConstant= new double[10];
+
+		BoltzmannConstant[0]=1.3244d;
+		BoltzmannConstant[1]=2.3244d;
+		BoltzmannConstant[2]=3.3244d;
+		BoltzmannConstant[3]=4.3244d;
+		BoltzmannConstant[4]=5.3244d;
+		BoltzmannConstant[5]=6.3244d;
+		BoltzmannConstant[6]=7.3244d;
+		BoltzmannConstant[7]=8.3244d;
+		BoltzmannConstant[8]=9.3244d;
+		BoltzmannConstant[9]=4.5544d;
+
+
+		System.out.println(BoltzmannConstant[0]);
+		System.out.println(BoltzmannConstant[1]);
+		System.out.println(BoltzmannConstant[2]);
+		System.out.println(BoltzmannConstant[3]);
+		System.out.println(BoltzmannConstant[4]);
+		System.out.println(BoltzmannConstant[5]);
+		System.out.println(BoltzmannConstant[6]);
+		System.out.println(BoltzmannConstant[7]);
+		System.out.println(BoltzmannConstant[8]);
+		System.out.println(BoltzmannConstant[9]);
+
+
+		char[] Alphabets={'A','B','C','D','E','F','G','H','I','J'};
+
+		Alphabets[0]='M';
+		Alphabets[1]='N';
+		Alphabets[5]='Z';
+
+		System.out.println(Alphabets[0]);
+		System.out.println(Alphabets[1]);
+		System.out.println(Alphabets[2]);
+		System.out.println(Alphabets[3]);
+		System.out.println(Alphabets[4]);
+		System.out.println(Alphabets[5]);
+		System.out.println(Alphabets[6]);
+		System.out.println(Alphabets[7]);
+		System.out.println(Alphabets[8]);
+		System.out.println(Alphabets[9]);
+
+		// key value method
+
+		char[] Numbers= new char[10];
+
+		Numbers[0]='1';
+		Numbers[2]='3';
+		Numbers[3]='5';
+		Numbers[4]='6';
+		Numbers[5]='8';
+		Numbers[6]='9';
+		Numbers[7]='0';
+		Numbers[8]='4';
+		Numbers[9]='2';
+		
+		System.out.println(Numbers[0]);
+		System.out.println(Numbers[1]);
+		System.out.println(Numbers[2]);
+		System.out.println(Numbers[3]);
+		System.out.println(Numbers[4]);
+		System.out.println(Numbers[5]);
+		System.out.println(Numbers[6]);
+		System.out.println(Numbers[7]);
+		System.out.println(Numbers[8]);
+		System.out.println(Numbers[9]);
+
+
+		String[] CollegeNames={"GECG","GECH","GECR","GECB","GECD","GECC","GECV","GECK","GECL","GECP"};
+
+		CollegeNames[0]="GECF";
+		CollegeNames[1]="GECM";
+		CollegeNames[3]="GECS";
+
+		System.out.println(CollegeNames[0]);
+		System.out.println(CollegeNames[1]);
+		System.out.println(CollegeNames[2]);
+		System.out.println(CollegeNames[3]);
+		System.out.println(CollegeNames[4]);
+		System.out.println(CollegeNames[5]);
+		System.out.println(CollegeNames[6]);
+		System.out.println(CollegeNames[7]);
+		System.out.println(CollegeNames[8]);
+		System.out.println(CollegeNames[9]);
+
+		// key value method
+
+		String[] PersonNames= new String[10];
+
+		PersonNames[0]="ramesh";
+		PersonNames[1]="shivu";
+		PersonNames[2]="praveen";
+		PersonNames[3]="naveen";
+		PersonNames[4]="tarun";
+		PersonNames[5]="hemanth";
+		PersonNames[6]="kalinga";
+		PersonNames[7]="venky";
+		PersonNames[8]="soma";
+		PersonNames[9]="pachhi";
+
+		System.out.println(PersonNames[0]);
+		System.out.println(PersonNames[1]);
+		System.out.println(PersonNames[2]);
+		System.out.println(PersonNames[3]);
+		System.out.println(PersonNames[4]);
+		System.out.println(PersonNames[5]);
+		System.out.println(PersonNames[6]);
+		System.out.println(PersonNames[6]);
+		System.out.println(PersonNames[7]);
+		System.out.println(PersonNames[8]);
+		System.out.println(PersonNames[9]);
+
+
+		boolean[] festival={true,false,true,true,false,true,false,false,true,true};
+
+
+		festival[0]=false;
+		festival[1]=true;
+		festival[2]=false;
+
+
+		System.out.println(festival[0]);
+		System.out.println(festival[1]);
+		System.out.println(festival[2]);
+		System.out.println(festival[3]);
+		System.out.println(festival[4]);
+		System.out.println(festival[5]);
+		System.out.println(festival[6]);
+		System.out.println(festival[7]);
+		System.out.println(festival[8]);
+		System.out.println(festival[9]);
+
+		// key value method
+
+		boolean[] college=new boolean[10];
+
+		college[0]=true;
+		college[1]=false;
+		college[2]=true;
+		college[3]=false;
+		college[4]=false;
+		college[5]=true;
+		college[6]=false;
+		college[7]=false;
+		college[8]=true;
+		college[9]=false;
+
+
+		System.out.println(college[0]);
+		System.out.println(college[1]);
+		System.out.println(college[2]);
+		System.out.println(college[3]);
+		System.out.println(college[4]);
+		System.out.println(college[5]);
+		System.out.println(college[6]);
+		System.out.println(college[7]);
+		System.out.println(college[8]);
+		System.out.println(college[9]);
+		
+		
+
+	}
+
+			
+}
